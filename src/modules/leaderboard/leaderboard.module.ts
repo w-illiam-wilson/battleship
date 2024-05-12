6a45712e7b6d430e55a29857c2d6a9b04ff7dc32
William Wilson <williambwilson@msn.com>
2024-05-12 19:56:01 -0400

    Update leaderboard.module.ts

--- src/modules/leaderboard/leaderboard.module.ts
+++ src/modules/leaderboard/leaderboard.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { LeaderboardController } from './leaderboard.controller';
 import { LeaderboardService } from './leaderboard.service';
 import { MatchModule } from '../match/match.module';
-import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [MatchModule, UserModule],
+  imports: [MatchModule],
   controllers: [LeaderboardController],
   providers: [LeaderboardService],
 })
