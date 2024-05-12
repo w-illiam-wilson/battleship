491a98253fac18c60963ddb30fb4821b78bbf3a9
William Wilson <williambwilson@msn.com>
2024-05-12 12:34:28 -0400

    Update board.controller.ts

--- src/modules/board/board.controller.ts
+++ src/modules/board/board.controller.ts
@@ -1,9 +1,8 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { BoardService } from './board.service';
 import { SetupDTO } from './entities/setup-dto.entity';
-import { ShipPiece } from './entities/ship.entity';
 import { MissileDTO } from './entities/fire-dto.entity';
-import { OpponentBoard, OpponentSquareCurrentState, ViewableBoard, YourBoard } from './entities/viewable-board-dto.entity';
+import { OpponentBoard, ViewableBoard, YourBoard } from './entities/viewable-board-dto.entity';
 import { BoardQuery } from './entities/dto/board-query.entity';
 
 @Controller("/boards")
@@ -19,17 +18,17 @@ export class BoardController {
 
   @Post()
   async setupBoard(
-    @Query("matchId") matchId: string,
+    @Query() boardQuery: BoardQuery,
     @Body() setup: SetupDTO
   ): Promise<YourBoard> {
-    return await this.boardService.setupBoard(matchId, setup);
+    return await this.boardService.setupBoard(boardQuery.matchId, setup);
   }
 
   @Post("/fire-missile")
   async getLeaderboard(
-    @Query("matchId") matchId: string,
+    @Query() boardQuery: BoardQuery,
     @Body() missile: MissileDTO
   ): Promise<OpponentBoard | string> {
-    return await this.boardService.fireMissile(matchId, missile);
+    return await this.boardService.fireMissile(boardQuery.matchId, missile);
   }
 }
